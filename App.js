841b20f9949f8f442ebca0e91aff9e1d0943c61e
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-04-23 14:16:33 -0500

    [WORKING] Audio streaming integration

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from './Navigation';
+import { Audio } from 'expo-av';
 
 export default function App() {
+	const [sound, setSound] = useState();
+
+	const playSound = async () => {
+		const { sound } = await Audio.Sound.createAsync(
+			require('./assets/sounds/initialSound.mp3')
+		);
+		setSound(sound);
+		await sound.playAsync();
+	};
+
+	useEffect(() => {
+		playSound();
+	}, []);
+
+	useEffect(() => {
+		return sound
+			? () => {
+					sound.unloadAsync();
+			  }
+			: undefined;
+	}, [sound]);
+
 	return <Navigation />;
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,63 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { Audio } from 'expo-av';
 
 const GOOGLE = 'https://www.google.com';
 
+/* <div style="background-color: red; height: 100%">TEST</div> */
+const STREAM_URI = 'http://stream.zeno.fm/m7e2znfd6nhvv';
+
 const Home = () => {
+	const [radio, setRadio] = useState();
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [isStarted, setIsStarted] = useState(false);
+
+	const playSound = async () => {
+		if (!radio && !isStarted) {
+			setIsStarted(true);
+			const { sound } = await Audio.Sound.createAsync({
+				uri: STREAM_URI,
+			});
+			setRadio(sound);
+			await sound.playAsync();
+			setIsPlaying(true);
+		} else {
+			if (isPlaying) {
+				await radio.stopAsync();
+				setIsPlaying(false);
+				console.log(radio);
+			} else {
+				await radio.playAsync();
+				setIsPlaying(true);
+			}
+		}
+		console.log(radio?.isPlaying());
+	};
+
+	// useEffect(() => {
+
+	// }, []);
+
+	useEffect(() => {
+		console.log(radio);
+	}, [radio]);
+
 	return (
-		<View style={{ flex: 1 }}>
-			<Text style={{ marginTop: 40 }}>HELLLOOOOOO</Text>
-			<WebView
-				source={{
-					html: '<iframe src="https://zeno.fm/player/maravilla-stereo" width="100%" height="500" frameborder="0" scrolling="no"></iframe><a href="https://zeno.fm/" target="_blank" style="display: block; font-size: 0.9em; line-height: 10px;">A Zeno.FM Station</a>',
-				}}
-				style={{ width: '100%', height: '100%' }}
+		<View style={styles.container}>
+			<Text style={styles.title}>HELLLOOOOOO</Text>
+			<Button
+				onPress={playSound}
+				title="Play Radio"
+				color="#841584"
+				accessibilityLabel="Botón para reproducir la radio"
 			/>
+			{/* <WebView
+				source={{
+					html: '<iframe src="https://zeno.fm/player/maravilla-stereo" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>',
+				}}
+				style={{ width: '100%', height: 50 }}
+			/> */}
 			{/* <StatusBar style="auto" /> */}
 		</View>
 	);
@@ -22,14 +66,13 @@ const Home = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#3333ac',
-		alignItems: 'center',
-		justifyContent: 'center',
+		// backgroundColor: '#3333ac',
 	},
 	title: {
 		fontSize: 30,
 		color: 'white',
 		fontWeight: 'bold',
+		marginTop: 40,
 	},
 	webView: {
 		// width: 400,
