50e28a089a01f9b92897d3b722f049864d8298e9
josegabjimenez <jose.vidales@correounivalle.edu.co>
2022-04-22 23:39:16 -0500

    Navigation done

---
+++ Navigation.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+// Screens
+import Home from './screens/Home';
+import Schedule from './screens/Schedule';
+
+// Icons
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const MyTabs = () => {
+	return (
+		<Tab.Navigator
+			initialRouteName="Escuchar"
+			screenOptions={{
+				tabBarActiveTintColor: 'green',
+			}}
+		>
+			<Tab.Screen
+				name="Home"
+				component={Home}
+				options={{
+					tabBarLabel: 'Escuchar',
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="radio" size={size} color={color} />
+					),
+					headerShown: false,
+				}}
+			/>
+			<Tab.Screen
+				name="Schedule"
+				component={Schedule}
+				options={{
+					tabBarLabel: 'Programación',
+					tabBarIcon: ({ color, size }) => (
+						<MaterialIcons name="schedule" size={size} color={color} />
+					),
+					headerShown: false,
+				}}
+			/>
+		</Tab.Navigator>
+	);
+};
+
+const Navigation = () => {
+	return (
+		<NavigationContainer>
+			<MyTabs />
+		</NavigationContainer>
+	);
+};
+
+export default Navigation;
